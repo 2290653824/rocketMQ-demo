0ad3da771548f5256e91071d5febcc82600c2970
zhengjian <zhengjian@qiniu.com>
2023-10-05 20:52:24 +0800

    init

---
+++ src/main/java/org/zj/fifo/Producer.java
@@ -0,0 +1,15 @@
+package org.zj.fifo;
+
+public class Producer {
+
+    public static final String PRODUCER_GROUP = "fifoGroup001";
+    public static final String DEFAULT_NAMESRVADDR = "127.0.0.1:9876";
+    public static final String TOPIC = "fifo_topic";
+    public static final String TAG = "fifo_a";
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
